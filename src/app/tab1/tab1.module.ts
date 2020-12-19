d33e649a6b7489c1c54353882febffe7e5b85cee
Oğuzhan Sarıçam <oguzhan.saricam@outlook.com.tr>
2020-12-19 15:41:02 +0300

    player

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,12 +3,14 @@ import { NgModule, OnInit } from '@angular/core';
 import { Tab1Page } from './tab1.page';
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     IonicModule,
     Tab1PageRoutingModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,9 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../dataservices/data.service';
 import { Files } from '../models/Files';
+import { Howl } from 'howler';
+import { IonRange } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,7 +13,12 @@ export class Tab1Page implements OnInit {
   
   public searchTerm: string = "";
   Files:Files[] = [];
-  activeFile:Files =null;
+
+  progress  = 0;
+  isplaying = false;
+  player:Howl = null;
+  activeFile:Files = null;
+  @ViewChild('range', {static:false})range: IonRange;
 
   constructor(private dataService: DataService) {}
 
@@ -39,26 +47,70 @@ export class Tab1Page implements OnInit {
   }
 
   start(file: Files){
-    // this.player = new Howl()
+    if(this.player){
+      this.player.stop();
+    }
+    this.player = new Howl({
+      src:[file.FileUrl],
+      html5:true,
+      onplay:()=>{
+        this.isplaying=true;
+        this.activeFile=file;
+        this.updateProgress();
+      },
+      onend:()=>{
+        this.isplaying=false;
+        this.activeFile=null;
+      }
+    });
+     this.player.play();
   }
 
   togglePlayer(pause){
-
+    if(this.player==null)
+    {
+      this.isplaying=false;
+      return;
+    }
+    this.isplaying = !pause;
+    if(pause)
+    {
+      this.player.pause();
+    }else{
+      this.player.play();
+    }
   }
 
   next(){
-
+    let index = this.Files.indexOf(this.activeFile);
+    if(index != this.Files.length-1){
+      this.start(this.Files[index+1]);
+    } else {
+      this.start(this.Files[0]);
+    }
   }
 
   prev(){
-
+    let index = this.Files.indexOf(this.activeFile);
+    if(index > 0){
+      this.start(this.Files[index-1]);
+    } else 
+    {
+      this.start(this.Files[this.Files.length -1]);
+    }
   }
 
   seek(){
-
+    let val = +this.range.value;
+    let dur = this.player.duration();
+    this.player.seek(dur * (val / 100));
   }
 
   updateProgress(){
-
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(()=> {
+      this.updateProgress();
+    }, 500);
   }
 }
